ad9aea554864491d0dec16c98680ec0138cd8383
adkrueger <adkrueger@wpi.edu>
2018-11-06 16:02:04 -0500

    wrote tests for getNumMisses

--- Tests/CacheTest.java
+++ Tests/CacheTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
  * Code to test an <tt>LRUCache</tt> implementation.
  */
 public class CacheTest {
+
+
 	@Test
 	public void testGetNumMisses () {
 		NameProvider nameProvider = new NameProvider();
@@ -14,7 +16,7 @@ public class CacheTest {
 		nameProvider.addName(2, "Joe");
 
 		DataProvider<Integer,String> provider = nameProvider; // Need to instantiate an actual DataProvider
-		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 5);
+		Cache<Integer,String> cache = new LRUCache<>(provider, 5);
 
 		assertEquals(cache.getNumMisses(), 0);
 		assertEquals(cache.get(0), "Bob");
@@ -30,7 +32,7 @@ public class CacheTest {
 	}
 
 	@Test
-    public void testLRU () {
+    public void testGetNumMisses2 () {
         NameProvider nameProvider = new NameProvider();
         nameProvider.addName(4, "Bob");
         nameProvider.addName(0, "Bill");
@@ -51,5 +53,60 @@ public class CacheTest {
         assertEquals(cache.get(1), "Zim");
         assertEquals(cache.get(4), "Bob"); // Shouldn't increase number of misses, as it's still in the cache
         assertEquals(cache.getNumMisses(), 7); // A total of 7 misses
+    }
+
+    @Test
+    public void testGetNumMisses3() {
+        NameProvider nameProvider = new NameProvider();
+        nameProvider.addName(4, "Bob");
+        nameProvider.addName(0, "Bill");
+        nameProvider.addName(3, "Joe");
+        nameProvider.addName(1, "Zim");
+        nameProvider.addName(2, "Brandon");
+
+        Cache<Integer, String> cache = new LRUCache<> (nameProvider, 3);
+
+        assertEquals(cache.get(0), "Bill");   // misses += 1
+        cache.get(0);
+        cache.get(0);
+        assertEquals(cache.get(0), "Bill");
+        // cache should still only have one person, so misses still = 1
+        assertEquals(cache.getNumMisses(), 1);
+        cache.get(1);   // new addition, so misses += 1
+        assertEquals(cache.getNumMisses(), 2);
+
+    }
+
+    @Test
+    public void testGetNumMisses4() {
+        NameProvider nameProvider = new NameProvider();
+        nameProvider.addName(4, "Bob");
+        nameProvider.addName(0, "Bill");
+        nameProvider.addName(3, "Joe");
+        nameProvider.addName(1, "Zim");
+        nameProvider.addName(2, "Brandon");
+
+        Cache<Integer, String> cache = new LRUCache<> (nameProvider, 3);
+
+        assertEquals(cache.get(4), "Bob");
+        assertEquals(cache.get(3), "Joe");
+        assertEquals(cache.get(0), "Bill");
+        // cache is now full, misses = 3
+        assertEquals(cache.get(2), "Brandon");   // Adds Brandon to the cache and removes Bob, misses = 4
+        assertEquals(cache.get(4), "Bob");       // Should add 1 to misses because Bob wasn't in cache anymore
+        assertEquals(cache.getNumMisses(), 5);
+    }
+
+    @Test
+    public void testLRUCache () {
+        NameProvider nameProvider = new NameProvider();
+        nameProvider.addName(0, "Liz");
+        nameProvider.addName(1, "Sadie");
+        nameProvider.addName(2, "Bailey");
+
+        DataProvider<Integer,String> provider = nameProvider;
+        Cache<Integer,String> cache = new LRUCache<>(provider, 3);
+
+        assertNull(cache.get(3));     // There is no 4th key, so should return null
     }
 }
