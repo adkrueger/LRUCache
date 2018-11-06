5d77f6764ad68e662359d91ee141e430192fbc80
rmanky <rmankaryous12@gmail.com>
2018-11-06 16:35:01 -0500

    Changed name provider to take a String as the key instead of an integer

--- Tests/CacheTest.java
+++ Tests/CacheTest.java
@@ -11,22 +11,22 @@ public class CacheTest {
 	@Test
 	public void testGetNumMisses () {
 		NameProvider nameProvider = new NameProvider();
-		nameProvider.addName(0, "Bob");
-		nameProvider.addName(1, "Bill");
-		nameProvider.addName(2, "Joe");
+		nameProvider.addName("bo", "Bob");
+		nameProvider.addName("bi", "Bill");
+		nameProvider.addName("jo", "Joe");
 
-		DataProvider<Integer,String> provider = nameProvider; // Need to instantiate an actual DataProvider
-		Cache<Integer,String> cache = new LRUCache<>(provider, 5);
+		DataProvider<String,String> provider = nameProvider; // Need to instantiate an actual DataProvider
+		Cache<String,String> cache = new LRUCache<>(provider,5);
 
 		assertEquals(cache.getNumMisses(), 0);
-		assertEquals(cache.get(0), "Bob");
-		assertEquals(cache.get(1), "Bill");
-		assertEquals(cache.get(2), "Joe");
+		assertEquals(cache.get("bo"), "Bob");
+		assertEquals(cache.get("bi"), "Bill");
+		assertEquals(cache.get("jo"), "Joe");
 		// Should have 3 misses by now
 		assertEquals(cache.getNumMisses(), 3);
-        assertEquals(cache.get(0), "Bob");
-        assertEquals(cache.get(1), "Bill");
-        assertEquals(cache.get(2), "Joe");
+        assertEquals(cache.get("bo"), "Bob");
+        assertEquals(cache.get("bi"), "Bill");
+        assertEquals(cache.get("jo"), "Joe");
         // Now there should still be 3 misses, as everything was in the cache
         assertEquals(cache.getNumMisses(), 3);
 	}
@@ -34,45 +34,45 @@ public class CacheTest {
 	@Test
     public void testGetNumMisses2 () {
         NameProvider nameProvider = new NameProvider();
-        nameProvider.addName(4, "Bob");
-        nameProvider.addName(0, "Bill");
-        nameProvider.addName(3, "Joe");
-        nameProvider.addName(1, "Zim");
-        nameProvider.addName(2, "Brandon");
+        nameProvider.addName("bo", "Bob");
+        nameProvider.addName("bi", "Bill");
+        nameProvider.addName("jo", "Joe");
+        nameProvider.addName("zi", "Zim");
+        nameProvider.addName("br", "Brandon");
 
-        DataProvider<Integer,String> provider = nameProvider;
-        Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 4); // Only four capacity
+        DataProvider<String,String> provider = nameProvider;
+        Cache<String,String> cache = new LRUCache<>(provider, 4); // Only four capacity
 
         assertEquals(cache.getNumMisses(), 0);
-        assertEquals(cache.get(0), "Bill");
-        assertEquals(cache.get(1), "Zim");
-        assertEquals(cache.get(2), "Brandon");
-        assertEquals(cache.get(3), "Joe");
-        assertEquals(cache.get(4), "Bob");
-        assertEquals(cache.get(0), "Bill");
-        assertEquals(cache.get(1), "Zim");
-        assertEquals(cache.get(4), "Bob"); // Shouldn't increase number of misses, as it's still in the cache
+        assertEquals(cache.get("bi"), "Bill");
+        assertEquals(cache.get("zi"), "Zim");
+        assertEquals(cache.get("br"), "Brandon");
+        assertEquals(cache.get("jo"), "Joe");
+        assertEquals(cache.get("bo"), "Bob");
+        assertEquals(cache.get("bi"), "Bill");
+        assertEquals(cache.get("zi"), "Zim");
+        assertEquals(cache.get("bo"), "Bob"); // Shouldn't increase number of misses, as it's still in the cache
         assertEquals(cache.getNumMisses(), 7); // A total of 7 misses
     }
 
     @Test
     public void testGetNumMisses3() {
         NameProvider nameProvider = new NameProvider();
-        nameProvider.addName(4, "Bob");
-        nameProvider.addName(0, "Bill");
-        nameProvider.addName(3, "Joe");
-        nameProvider.addName(1, "Zim");
-        nameProvider.addName(2, "Brandon");
+        nameProvider.addName("bo", "Bob");
+        nameProvider.addName("bi", "Bill");
+        nameProvider.addName("jo", "Joe");
+        nameProvider.addName("zi", "Zim");
+        nameProvider.addName("br", "Brandon");
 
-        Cache<Integer, String> cache = new LRUCache<> (nameProvider, 3);
+        Cache<String, String> cache = new LRUCache<> (nameProvider, 3);
 
-        assertEquals(cache.get(0), "Bill");   // misses += 1
-        cache.get(0);
-        cache.get(0);
-        assertEquals(cache.get(0), "Bill");
+        assertEquals(cache.get("bi"), "Bill");   // misses += 1
+        cache.get("bi");
+        cache.get("bi");
+        assertEquals(cache.get("bi"), "Bill");
         // cache should still only have one person, so misses still = 1
         assertEquals(cache.getNumMisses(), 1);
-        cache.get(1);   // new addition, so misses += 1
+        cache.get("bo");   // new addition, so misses += 1
         assertEquals(cache.getNumMisses(), 2);
 
     }
@@ -80,33 +80,33 @@ public class CacheTest {
     @Test
     public void testGetNumMisses4() {
         NameProvider nameProvider = new NameProvider();
-        nameProvider.addName(4, "Bob");
-        nameProvider.addName(0, "Bill");
-        nameProvider.addName(3, "Joe");
-        nameProvider.addName(1, "Zim");
-        nameProvider.addName(2, "Brandon");
+        nameProvider.addName("bo", "Bob");
+        nameProvider.addName("bi", "Bill");
+        nameProvider.addName("jo", "Joe");
+        nameProvider.addName("zi", "Zim");
+        nameProvider.addName("br", "Brandon");
 
-        Cache<Integer, String> cache = new LRUCache<> (nameProvider, 3);
+        Cache<String, String> cache = new LRUCache<> (nameProvider, 3);
 
-        assertEquals(cache.get(4), "Bob");
-        assertEquals(cache.get(3), "Joe");
-        assertEquals(cache.get(0), "Bill");
+        assertEquals(cache.get("bo"), "Bob");
+        assertEquals(cache.get("jo"), "Joe");
+        assertEquals(cache.get("bi"), "Bill");
         // cache is now full, misses = 3
-        assertEquals(cache.get(2), "Brandon");   // Adds Brandon to the cache and removes Bob, misses = 4
-        assertEquals(cache.get(4), "Bob");       // Should add 1 to misses because Bob wasn't in cache anymore
+        assertEquals(cache.get("br"), "Brandon");   // Adds Brandon to the cache and removes Bob, misses = 4
+        assertEquals(cache.get("bo"), "Bob");       // Should add 1 to misses because Bob wasn't in cache anymore
         assertEquals(cache.getNumMisses(), 5);
     }
 
     @Test
     public void testLRUCache () {
         NameProvider nameProvider = new NameProvider();
-        nameProvider.addName(0, "Liz");
-        nameProvider.addName(1, "Sadie");
-        nameProvider.addName(2, "Bailey");
+        nameProvider.addName("li", "Liz");
+        nameProvider.addName("sa", "Sadie");
+        nameProvider.addName("ba", "Bailey");
 
-        DataProvider<Integer,String> provider = nameProvider;
-        Cache<Integer,String> cache = new LRUCache<>(provider, 3);
+        DataProvider<String,String> provider = nameProvider;
+        Cache<String,String> cache = new LRUCache<>(provider, 3);
 
-        assertNull(cache.get(3));     // There is no 4th key, so should return null
+        assertNull(cache.get("zz"));     // There is no 4th key, so should return null
     }
 }
--- src/NameProvider.java
+++ src/NameProvider.java
@@ -1,30 +1,17 @@
 import java.util.HashMap;
 
-/**
- * An implementation of a class that utilizes a hashMap to store data
- */
-public class NameProvider implements DataProvider<Integer, String> {
+public class NameProvider implements DataProvider<String, String> {
 
-    private HashMap<Integer, String> map = new HashMap<>();
+    private HashMap<String, String> map = new HashMap<>();
 
-    /**
-     * Returns the key value of a hashMap
-     * @param key the key
-     * @return the key value
-     */
-    public String get (Integer key) {
+    public String get (String key) {
         return map.get(key);
     }
 
-    /**
-     * Adds a key and value to the hashMap
-     * @param key the key value of the data
-     * @param name the corresponding name data
-     */
-    public void addName(Integer key, String name)
+    public void addName(String key, String name)
     {
         if(map.containsKey(key)) {
-            throw new Error("Key already exists in the hashMap");
+            throw new Error("Key already exists in the hashmap");
         }
         else
         {
